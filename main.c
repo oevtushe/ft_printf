97f7e31a65a3cec2fc571ca7d63740354fe3dec3
oevtushe <casio6290@gmail.com>
2018-01-02 20:31:46 +0200

    debug file

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "ft_printf.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 int		main(int argc, char **argv)
 {
@@ -7,6 +8,6 @@ int		main(int argc, char **argv)
 		return (1);
 	ft_printf(argv[1], atol(argv[2]));
 	printf("\n");
-	printf(argv[1], ft_atoi(argv[2]));
+	printf(argv[1], atol(argv[2]));
 	return (0);
 }
