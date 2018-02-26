137b3ecfe0d38da84629901b359570e69d4a9593
oevtushe <casio6290@gmail.com>
2018-02-26 18:47:22 +0200

    Use ft_memdel instead free.

--- tools/ft_lstaddelem.c
+++ tools/ft_lstaddelem.c
@@ -13,11 +13,19 @@
 #include <stdlib.h>
 #include "ft_tools.h"
 
+/*
+** Add element at list then free that element.
+**
+** @param	lst				list element be added to.
+** @param	content			content of a new element.
+** @param	content_size	content size of a new element.
+*/
+
 void	ft_lstaddelem(t_list **lst, void **content, size_t content_size)
 {
 	t_list	*new_elem;
 
 	new_elem = ft_lstnew(*content, content_size);
 	ft_lstappend(lst, new_elem);
-	free(*content);
+	ft_memdel(content);
 }
