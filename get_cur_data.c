8423bf2b49046a1be423ed4a2ee56801c5976c49
oevtushe <casio6290@gmail.com>
2018-02-26 17:45:30 +0200

    Added castilla for handling T_PT('%') as real type.

--- get_cur_data.c
+++ get_cur_data.c
@@ -13,14 +13,23 @@
 #include "ft_printf_helpers.h"
 
 /*
-** function returns data from @param gdata arr by index readed from @param str
+** Function returns data from @param gdata arr by index readed from @param str
 ** in case of '$' appearence or by index @param di.
+** If is '%' type returns new gdata object appropriately initiated.
+**
+** @param	str		format string.
+** @param	idx		current index in format string to check from.
+** @param	di		current index in gdata array.
+** @param	gd		gdata array.
+**
+** @return			gdata loaded from array.
 */
 
 t_gdata		*get_cur_data(const char *str, size_t *idx, int *di, t_gdata **gd)
 {
-	int j;
-	int ndi;
+	int		j;
+	int		ndi;
+	t_gdata *gdata;
 
 	j = *idx;
 	if (ft_isdigit(str[j]))
@@ -33,6 +42,12 @@ t_gdata		*get_cur_data(const char *str, size_t *idx, int *di, t_gdata **gd)
 			*di = ndi;
 			*idx = ++j;
 		}
+	}
+	if (str[ft_strlen(str) - 1] == T_PT)
+	{
+		gdata = new_gdata();
+		gdata->full_type->type = T_PT;
+		return (gdata);
 	}
 	return (gd[(*di)++]);
 }
