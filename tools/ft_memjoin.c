df50eb02ac59a6adb3276f3157ec92350e84cd52
oevtushe <casio6290@gmail.com>
2018-03-03 01:23:20 +0200

    Using ft_memalloc instead malloc.

--- tools/ft_memjoin.c
+++ tools/ft_memjoin.c
@@ -28,7 +28,7 @@ void	*ft_memjoin(void *p1, size_t sp1, void *p2, size_t sp2)
 {
 	unsigned char *res;
 
-	res = (unsigned char *)malloc(sizeof(unsigned char) * (sp1 + sp2));
+	res = (unsigned char *)ft_memalloc(sizeof(unsigned char) * (sp1 + sp2));
 	if (res)
 	{
 		ft_memcpy(res, p1, sp1);
